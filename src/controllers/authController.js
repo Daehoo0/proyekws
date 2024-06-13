fecd6550d33bcf83be94aa65ebe43f8aba4276ba
RickyPrakusaW <133295075+RickyPrakusaW@users.noreply.github.com>
2024-06-14 05:39:17 +0700

    fix bug

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -178,7 +178,7 @@ const loginUser = async (req, res) => {
       });
     }
 
-    // Generate JWT token 
+    // Generate JWT token
     const token = jwt.sign(
       { id: user.user_id, role: user.role },
       process.env.JWT_SECRET, // Use the secret key from .env
