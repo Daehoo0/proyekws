6f0abe3a52259762d11b49f326930dfa234fe3cf
Steven Sean <133088404+Steven-Sean@users.noreply.github.com>
2024-06-14 08:56:01 +0700

    prettier (again)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 const database = require("./config/sequelize");
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 const multer = require("./config/multer");
-const authRoutes = require('./routes/authRoutes');
+const authRoutes = require("./routes/authRoutes");
 dotenv.config();
 
 const {
@@ -22,28 +22,45 @@ const {
   cancelEventRegistration,
 } = require("./controllers/userController");
 
-const { verifyToken, verifyAccess } = require("./middlewares/authMiddleware"); 
+const { verifyToken, verifyAccess } = require("./middlewares/authMiddleware");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api', authRoutes);
+app.use("/api", authRoutes);
 
 app.get("/api/getAirport", [verifyToken, verifyAccess], getAirport);
 app.post("/api/recharge", [verifyToken], recharge);
 app.get("/api/findPlace", findPlace);
 app.get("/api/events", [verifyToken, verifyAccess], getEvents);
-app.post('/api/reviews', [verifyToken, verifyAccess], addReview); 
-app.get('/api/reviews', [verifyToken, verifyAccess], getReviewsByUser); 
-app.put('/api/reviews', [verifyToken, verifyAccess], updateReview); 
+app.post("/api/reviews", [verifyToken, verifyAccess], addReview);
+app.get("/api/reviews", [verifyToken, verifyAccess], getReviewsByUser);
+app.put("/api/reviews", [verifyToken, verifyAccess], updateReview);
 app.get("/api/destination", getDestination);
-app.delete('/api/guideProfile', [verifyToken, verifyAccess], deleteGuideProfile);
-app.post('/api/events/:event_id/register', [verifyToken, verifyAccess], registerForEvent);
-app.delete('/api/events/:event_id/unregister', [verifyToken, verifyAccess], cancelEventRegistration);
+app.delete(
+  "/api/guideProfile",
+  [verifyToken, verifyAccess],
+  deleteGuideProfile
+);
+app.post(
+  "/api/events/:event_id/register",
+  [verifyToken, verifyAccess],
+  registerForEvent
+);
+app.delete(
+  "/api/events/:event_id/unregister",
+  [verifyToken, verifyAccess],
+  cancelEventRegistration
+);
 
-app.put('/api/guideProfile', [verifyToken, verifyAccess], multer.single('photo'), updateGuideProfile);
+app.put(
+  "/api/guideProfile",
+  [verifyToken, verifyAccess],
+  multer.single("photo"),
+  updateGuideProfile
+);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const init = async () => {
   console.log("Testing connection to the database");
   try {
